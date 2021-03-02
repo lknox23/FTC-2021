d5c87292b6da1ee7a36defd062280e5dd6ddb7e3
lknox23 <56613012+lknox23@users.noreply.github.com>
2021-03-01 23:20:13 -0800

    Create MecanumAuto1.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/autonomous/MecanumAuto1.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.drive.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+public class MecanumAuto1 extends LinearOpMode {
+    SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+    @Override
+    public void runOpMode() {
+
+    }
+}
